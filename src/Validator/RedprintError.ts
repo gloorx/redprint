5be656ffeb06c8a2f0ebb8f5523eab0f6e0d478b
gloorx <glrx.vloq@gmail.com>
2017-09-11 01:17:35 +0900

    Add more property to RedprintError

--- src/Validator/RedprintError.ts
+++ src/Validator/RedprintError.ts
@@ -1,7 +1,14 @@
 export class RedprintError extends Error {
+  public input: any;
+  public key: string;
+  public validationName: string;
   constructor(input: any, key: string, validationName: string) {
-    const msg = `'${input}' is invalid ${key} for '${validationName}' validation`;
-    super(msg);
+    const message = `'${input}' is invalid ${key} for '${validationName}' validation`;
+    super(message);
+
+    this.input = input;
+    this.key = key;
+    this.validationName = validationName;
 
     Object.setPrototypeOf(this, RedprintError.prototype);
   }
